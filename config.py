b01db1c4c71d1ac1683a42b28a5f2cf76000c55e
natpio <252204917+natpio@users.noreply.github.com>
2026-05-08 13:25:02 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,29 @@
+# config.py
+
+LISTA_AUT = [
+    "Jumper – PY22952",
+    "Jumper – PY22954",
+    "BOXER - PO 5VT68",
+    "BOXER - WZ213GF",
+    "BOXER - WZ214GF",
+    "BOXER - WZ215GF",
+    "OPEL DW4WK43",
+    "BOXER (WYPAS) DW7WE24",
+    "OPEL wysoki DW4WK45",
+    "BOXER DW9WK54 wysoki",
+    "OPEL DW9WK53",
+    "SOLO PY 73262"
+]
+
+TYPY_AKCJI = [
+    "Dowóz - Rental",
+    "Odbiór - Rental",
+    "Dowóz - Realizacja",
+    "Odbiór - Realizacja",
+    "Magazyn: Odbiór przez klienta - Rental",
+    "Magazyn: Zwrot przez klienta - Rental",
+    "Magazyn: Odbiór przez klienta - Realizacja",
+    "Magazyn: Zwrot przez klienta - Realizacja"
+]
+
+DZIALY = ["Rental", "Realizacja"]
